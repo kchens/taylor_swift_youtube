1a82c6967e6b9af1635df2a23d55ec2d51b47c1f
Meg Sandman <margaret.sandman@gmail.com>
2014-11-03 23:20:19 -0800

    Revert "Megfriday"

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :actions
-  has_many :products, through: :actions
-
-  validates :name, :email, :password,  presence: true
-  validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }
-end
-
