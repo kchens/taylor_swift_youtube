55c6570cd497c8d23d5de7654f67449aa9c2d103
Kevin K Chen <kevin.kang.chen@gmail.com>
2014-11-13 12:18:42 -0800

    refacoted like love ajax

--- public/js/application.js
+++ public/js/application.js
@@ -3,43 +3,34 @@ $(document).ready(function() {
 });
 
 function bindEvents() {
-  $(".vid-info").on("click", "#like-video", addLike);
-  $(".vid-info").on("click", "#love-video", addLove);
+  $(".vid-info").on("click", incrementLikeLove);
 }
 
-
-function addLike(e) {
+function incrementLikeLove(e) {
   e.preventDefault();
+  var $thumbOrHeart = $(e.target);
 
-  var $likeThumb = $(e.target);
-  var urlPath = $likeThumb.parent().attr("href");
-  console.log(urlPath)
+  var urlPath = $thumbOrHeart.parent().attr("href");
+  var id = $thumbOrHeart.parent().attr('id')
+  if (id === "like-video") {
+    var data = {button: "like-video"};
+  } else {
+    var data = {button: "love-video"};
+  }
+
   ajaxRequest = $.ajax({
     url: urlPath,
-    type: "POST"
+    type: "POST",
+    data: data
   });
   ajaxRequest.done( function(e) {
-    $likeThumb.text(e)
+    $thumbOrHeart.text(e)
   });
   ajaxRequest.fail( function(e) {
-    alert("AJAX Failure: You mean you LOVE this video?")
-  })
-}
-
-function addLove(e) {
-  e.preventDefault();
-
-  var $loveHeart = $(e.target);
-  var urlPath = $loveHeart.parent().attr("href");
-  console.log(urlPath)
-  ajaxRequest = $.ajax({
-    url: urlPath,
-    type: "POST"
-  });
-  ajaxRequest.done( function(e) {
-    $loveHeart.text(e)
-  });
-  ajaxRequest.fail( function(e) {
-    alert("AJAX Failure: No heart can encapsulate your love for TSwizzle.")
+    if (id === "like-video") {
+      alert("AJAX Failure: You mean you LOVE this video?")
+    } else {
+      alert("AJAX Failure: No heart can encapsulate your love for TSwizzle.")
+    }
   })
 }
