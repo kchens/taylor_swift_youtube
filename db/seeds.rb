7950f3e1c60e340ccd75c585369f5b7ab2404176
Kevin K Chen <kevin.kang.chen@gmail.com>
2014-11-14 12:24:45 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-categories = ["Hits", "Live", "Country", "Pop"] # , "Winnie The Pooh"
+categories = ["Hits", "Live", "Country", "Pop", "Winnie The Pooh"]
 
 categories.each do |category_name|
   Category.create(name: category_name)
 end
 
-# @category = Category.find_by(name: "Winnie The Pooh")
-# @search = true
-# @all_videos = Video.get_all_video_info("#{@category.name}", @search)
-# @category.save_to_database(@all_videos)
-# @videos = @category.videos
+@category = Category.find_by(name: "Winnie The Pooh")
+@search = true
+@all_videos = Video.get_all_video_info("#{@category.name}", @search)
+@category.save_to_database(@all_videos)
+@videos = @category.videos
